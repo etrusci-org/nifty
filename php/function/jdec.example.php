2c7b89f3d4115776ce6b1641b911319509790075
arT2 <60965610+etrusci-org@users.noreply.github.com>
2022-05-15 17:25:38 +0200

    Usability improvement.

--- php/function/jdec.example.php
+++ php/function/jdec.example.php
@@ -3,6 +3,7 @@ require __DIR__.'/jdec.php';
 
 $data1 = jdec('{"foo": "bar", "prime": 13}');
 $data2 = jdec('invalid json data');
+$data3 = jdec(null);
 
 var_dump($data1);
 // array(2) {
@@ -13,4 +14,7 @@ var_dump($data1);
 //   }
 
 var_dump($data2);
+// NULL
+
+var_dump($data3);
 // NULL
