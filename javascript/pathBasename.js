123f9fc835a013c9810413709cb972a5ac3ffc0b
arT2 <60965610+etrusci-org@users.noreply.github.com>
2022-06-10 18:25:49 +0200

    Started to add Typescript.

---
+++ javascript/pathBasename.js
@@ -0,0 +1,17 @@
+"use strict";
+/**
+ * Get only the trailing name component of a path.
+ *
+ * @param {string} path  Path to parse.
+ * @param {string} [sep='/']  Path separator.
+ * @param {boolean} [ignoreTrailingSep=true]  Whether to ignore a trailing path separator.
+ * @returns {string}  Trailing name component.
+ *
+ * @example pathBasename.example.html
+ */
+function pathBasename(path, sep = '/', ignoreTrailingSep = true) {
+    if (ignoreTrailingSep && path.endsWith(sep)) {
+        path = path.slice(0, -1);
+    }
+    return path.split(sep).pop() || path;
+}
