8a291da7742ac4192a0beb600244e06fcc945b78
arT2 <60965610+etrusci-org@users.noreply.github.com>
2022-06-21 13:41:50 +0200

    Renamed autoBlankExtHosts to addTargetToExtLinks.

--- javascript/addTargetToExtLinks.js
+++ javascript/addTargetToExtLinks.js
@@ -1,14 +1,15 @@
 /**
- * Automagically add target="_blank" to anchors that link to a different place.
+ * Automagically add target="..." to anchors that link to a different place.
  *
  * @param {array} localHostnames  Hostnames in this array will be kept alone.
+ * @param {string} [target='_blank']  Link target.
  * @returns {void}
  */
-export function autoBlankExtHost(localHostnames) {
+export function addTargetToExtLinks(localHostnames, target = '_blank') {
     let anchors = document.querySelectorAll('a');
     anchors.forEach(e => {
         if (localHostnames.indexOf(e.hostname) == -1) {
-            e.setAttribute('target', '_blank');
+            e.setAttribute('target', target);
         }
     });
 }
