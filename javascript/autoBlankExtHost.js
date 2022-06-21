2aa47be0826f18b900411371c2b7e19734be77c7
arT2 <60965610+etrusci-org@users.noreply.github.com>
2022-06-21 13:35:27 +0200

    Added autoBlankExtHosts.ts/js.

---
+++ javascript/autoBlankExtHost.js
@@ -0,0 +1,14 @@
+/**
+ * Automagically add target="_blank" to anchors that link to a different place.
+ *
+ * @param {array} localHostnames  Hostnames in this array will be kept alone.
+ * @returns {void}
+ */
+export function autoBlankExtHost(localHostnames) {
+    let anchors = document.querySelectorAll('a');
+    anchors.forEach(e => {
+        if (localHostnames.indexOf(e.hostname) == -1) {
+            e.setAttribute('target', '_blank');
+        }
+    });
+}
