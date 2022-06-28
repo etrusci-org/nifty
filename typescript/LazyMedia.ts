dff1723c60d321990c984f7a39384ed7c721d08d
arT2 <60965610+etrusci-org@users.noreply.github.com>
2022-06-28 11:47:24 +0200

    Rewrite start.

--- typescript/LazyMedia.ts
+++ typescript/LazyMedia.ts
@@ -1,238 +1,169 @@
-/**
- * Easily load lazy embeds.
- *
- * WORK IN PROGRESS!!
- */
- export const LazyMedia: LazyMediaInterface = {
-    selector: '.lazymedia',
-    slugSuffix: {
-        generic: {
-            link: '',
-            image: '',
-            audio: '',
-            video: '',
-        },
-        bandcamp: {
-            track: '/size=large/bgcol=ffffff/linkcol=0687f5/artwork=none/transparent=true/tracklist=false/',
-            album: '/size=large/bgcol=ffffff/linkcol=0687f5/artwork=none/transparent=true/',
-        },
-        mixcloud: {
-            mix: '&hide_cover=1',
-            playlist: '&hide_cover=1',
-        },
-        youtube: {
-            video: '?modestbranding=1&rel=0',
-            playlist: '&modestbranding=1&rel=0',
-        },
-    },
-    bandcampAlbumAutoHeight: {
-        header: 120,
-        trackRow: 33,
-        bottomBar: 50,
-    },
+interface LazyMediaInterface {
+    selector: string
+    slugTpl: {
+        link: string
+        image: string
+        audio: string
+        video: string
+        bandcampTrack: string
+        bandcampAlbum: string
+        mixcloudMix: string
+        mixcloudPlaylist: string
+        youtubeVideo: string
+        youtubePlaylist: string
+    }
+    load(): void
+    bakeElement(code: lazyCodeType, targetNode?: HTMLElement): HTMLElement | null
+    // slug(type: string, slug: string): string
+}
 
 
-    embed() {
+type lazyCodeType = {
+    type: string
+    slug: string
+}
+
+
+
+
+
+
+export const LazyMedia: LazyMediaInterface = {
+    selector: '.lazycode',
+
+    slugTpl: {
+        link: '{slug}',
+        image: '{slug}',
+        audio: '{slug}',
+        video: '{slug}',
+        bandcampTrack: '//bandcamp.com/EmbeddedPlayer/track={slug}/size=large/bgcol=ffffff/linkcol=0687f5/artwork=none/transparent=true/tracklist=false/',
+        bandcampAlbum: '//bandcamp.com/EmbeddedPlayer/album={slug}/size=large/bgcol=ffffff/linkcol=0687f5/artwork=none/transparent=true/',
+        mixcloudMix: '//www.mixcloud.com/widget/iframe/?feed={slug}&hide_cover=1',
+        mixcloudPlaylist: '//www.mixcloud.com/widget/iframe/?feed={slug}&hide_cover=1',
+        youtubeVideo: '//www.youtube.com/embed/{slug}?modestbranding=1&rel=0',
+        youtubePlaylist: '//www.youtube.com/embed/videoseries?list={slug}&modestbranding=1&rel=0',
+    },
+
+    load() {
         let nodes = document.querySelectorAll(this.selector)
-
-        if (!nodes) {
-            console.error('bad nodes:', nodes)
-            return
-        }
 
         nodes.forEach(node => {
             if (node instanceof HTMLElement) {
-
-                let code: lazyCodeType = null
-
                 try {
-                    code = JSON.parse(node.innerHTML)
-
-                    if (!code || !code.platform || !code.type || !code.slug) {
-                        code = null
-                        throw 'invalid JSON or missing platform, type or slug'
+                    let code: lazyCodeType = JSON.parse(node.innerHTML)
+                    let e = this.bakeElement(code, node)
+                    if (e) {
+                        console.debug('code:', JSON.stringify(code), '\ne:', e)
+                        node.replaceWith(e)
                     }
                 }
                 catch (error) {
-                    console.error('bad code:', node.innerHTML, 'in node:', node, 'error message:', error)
-                }
-
-                if (code) {
-                    // ----- CREATE BASIC ELEMENT DEPENDING ON PLATFORM AND TYPE -----
-                    let e = null
-
-                    // GENERIC
-                    if (code.platform == 'generic') {
-                        // link
-                        if (code.type == 'link') {
-                            e = this.createDefaultElement('a', node)
-                            e.setAttribute('href', `${code.slug}${this.slugSuffix.generic.link}`)
-                            e.innerText = (code.text) ? code.text : code.slug
-                        }
-
-                        // image
-                        if (code.type == 'image') {
-                            e = this.createDefaultElement('img', node)
-                            e.setAttribute('src', `${code.slug}${this.slugSuffix.generic.image}`)
-                            e.setAttribute('alt', code.slug.split('/').pop() || code.slug)
-                        }
-
-                        // audio
-                        if (code.type == 'audio') {
-                            e = this.createDefaultElement('audio', node)
-                            e.setAttribute('preload', 'metadata')
-                            e.setAttribute('controls', 'true')
-
-                            let e2 = this.createDefaultElement('source')
-                            e2.setAttribute('src', `${code.slug}${this.slugSuffix.generic.audio}`)
-
-                            let audioType = null
-                            let audioExt = code.slug.split('.').pop() || code.slug
-
-                            if (audioExt == 'mp3') {
-                                audioType = 'audio/mpeg'
-                            }
-
-                            if (audioExt == 'mp4') {
-                                audioType = 'audio/mp4'
-                            }
-
-                            if (audioType) {
-                                e2.setAttribute('type', audioType)
-                            }
-
-                            if (!code.noAudioLabel) {
-                                let e3 = this.createDefaultElement('label')
-                                e3.innerText = code.slug.split('/').pop() || code.slug
-                                node.insertAdjacentElement('beforebegin', e3)
-                            }
-
-                            e.appendChild(e2)
-                        }
-
-                        // video
-                        if (code.type == 'video') {
-                            e = this.createDefaultElement('video', node)
-                            e.setAttribute('preload', 'metadata')
-                            e.setAttribute('controls', 'true')
-
-                            let e2 = this.createDefaultElement('source')
-                            e2.setAttribute('src', `${code.slug}${this.slugSuffix.generic.video}`)
-
-                            let videoType = null
-                            let videoExt = code.slug.split('.').pop() || code.slug
-
-                            if (videoExt == 'mp4') {
-                                videoType = 'video/mp4'
-                            }
-
-                            if (videoExt == 'webm') {
-                                videoType = 'video/webm'
-                            }
-
-                            if (videoExt == 'ogv') {
-                                videoType = 'video/ogg'
-                            }
-
-                            if (videoType) {
-                                e2.setAttribute('type', videoType)
-                            }
-
-                            e.appendChild(e2)
-                        }
-                    }
-
-                    // BANDCAMP
-                    if (code.platform == 'bandcamp') {
-                        e = this.createDefaultElement('iframe', node)
-
-                        // track
-                        if (code.type == 'track') {
-                            e.setAttribute('src', `//bandcamp.com/EmbeddedPlayer/track=${code.slug}${this.slugSuffix.bandcamp.track}`)
-                        }
-
-                        // album
-                        if (code.type == 'album') {
-                            e.setAttribute('src', `//bandcamp.com/EmbeddedPlayer/album=${code.slug}${this.slugSuffix.bandcamp.album}`)
-
-                            if (code.trackCount) {
-                                e.style.height = `${Math.round(this.bandcampAlbumAutoHeight.header + (this.bandcampAlbumAutoHeight.trackRow * code.trackCount) + this.bandcampAlbumAutoHeight.bottomBar)}px`
-                            }
-                        }
-                    }
-
-                    // MIXCLOUD
-                    if (code.platform == 'mixcloud') {
-                        e = this.createDefaultElement('iframe', node)
-
-                        if (code.type == 'mix') {
-                            e.setAttribute('src', `//www.mixcloud.com/widget/iframe/?feed=${code.slug}${this.slugSuffix.mixcloud.mix}`)
-                        }
-
-                        if (code.type == 'playlist') {
-                            e.setAttribute('src', `//www.mixcloud.com/widget/iframe/?feed=${code.slug}${this.slugSuffix.mixcloud.playlist}`)
-                        }
-                    }
-
-                    // YOUTUBE
-                    if (code.platform == 'youtube') {
-                        e = this.createDefaultElement('iframe', node)
-                        e.setAttribute('allowfullscreen', 'true')
-
-                        if (code.type == 'video') {
-                            e.setAttribute('src', `//www.youtube.com/embed/${code.slug}${this.slugSuffix.youtube.video}`)
-                        }
-
-                        if (code.type == 'playlist') {
-                            e.setAttribute('src', `//www.youtube.com/embed/videoseries?list=${code.slug}${this.slugSuffix.youtube.playlist}`)
-                        }
-                    }
-
-                    // ----- POST PROCESS CREATED ELEMENT -----
-                    if (e) {
-                        e.classList.add(code.platform, code.type)
-
-                        // attribute
-                        if (code.attribute) {
-                            for (const [k, v] of code.attribute) {
-                                if (v != 'false') {
-                                    e.setAttribute(k, v)
-                                }
-                                else {
-                                    e.removeAttribute(k)
-                                }
-                            }
-                        }
-
-                        // dataset
-                        if (code.dataset) {
-                            for (const [k, v] of code.dataset) {
-                                e.dataset[k] = v
-                            }
-                        }
-
-                        // EMBED FINAL THING
-                        console.debug('[lazymedia]', 'code:', node.innerHTML, 'baked element:', e)
-                        node.replaceWith(e)
-                    }
+                    console.error('BAD CODE:', node.innerHTML, 'IN NODE:', node, 'ERROR MESSAGE:', error)
                 }
             }
         })
     },
 
 
-    createDefaultElement(tag, targetNode) {
-        let e = document.createElement(tag)
+    bakeElement(code, targetNode) {
+        let e = null
 
-        // add default attributes for specific elements
-        if (tag == 'iframe' || tag == 'img') {
-            e.setAttribute('loading', 'lazy')
+        // LINK
+        // FIXME: how to prevent `&` being transformed to `&amp;` for the href attribute without using replace()
+        if (code.type == 'link') {
+            e = document.createElement('a')
+
+            e.setAttribute('href', this.slugTpl.link.replace('{slug}', code.slug.replace('&amp;', '&')))
+            e.innerHTML = this.slugTpl.link.replace('{slug}', code.slug)
         }
 
-        // process further if target node is given
-        if (targetNode) {
-            // keep targetNode dataset
-            e.classList.add('lazymedia', ...targetNode.classList)
+        // IMAGE
+        if (code.type == 'image') {
+            e = document.createElement('img')
+
+            e.setAttribute('src', this.slugTpl.image.replace('{slug}', code.slug))
+        }
+
+        // AUDIO
+        if (code.type == 'audio') {
+            e = document.createElement('audio')
+            let e2 = document.createElement('source')
+
+            e.setAttribute('preload', 'metadata')
+            e.setAttribute('controls', 'controls')
+
+            e2.setAttribute('src', this.slugTpl.audio.replace('{slug}', code.slug))
+            let audioType = getAudioTypeByExt(code.slug)
+            if (audioType) {
+                e2.setAttribute('type', audioType)
+            }
+
+            e.appendChild(e2)
+        }
+
+        // VIDEO
+        if (code.type == 'video') {
+            e = document.createElement('video')
+            let e2 = document.createElement('source')
+
+            e.setAttribute('preload', 'metadata')
+            e.setAttribute('controls', 'controls')
+
+            e2.setAttribute('src', this.slugTpl.video.replace('{slug}', code.slug))
+            let videoType = getVideoTypeByExt(code.slug)
+            if (videoType) {
+                e2.setAttribute('type', videoType)
+            }
+
+            e.appendChild(e2)
+        }
+
+        // BANDCAMP
+        if (code.type == 'bandcampTrack') {
+            e = document.createElement('iframe')
+
+            e.setAttribute('src', this.slugTpl.bandcampTrack.replace('{slug}', code.slug))
+        }
+
+        if (code.type == 'bandcampAlbum') {
+            e = document.createElement('iframe')
+
+            e.setAttribute('src', this.slugTpl.bandcampAlbum.replace('{slug}', code.slug))
+        }
+
+        // MIXCLOUD
+        if (code.type == 'mixcloudMix') {
+            e = document.createElement('iframe')
+
+            e.setAttribute('src', this.slugTpl.mixcloudMix.replace('{slug}', code.slug))
+        }
+
+        if (code.type == 'mixcloudPlaylist') {
+            e = document.createElement('iframe')
+
+            e.setAttribute('src', this.slugTpl.mixcloudPlaylist.replace('{slug}', code.slug))
+        }
+
+        // YOUTUBE
+        if (code.type == 'youtubeVideo') {
+            e = document.createElement('iframe')
+
+            e.setAttribute('src', this.slugTpl.youtubeVideo.replace('{slug}', code.slug))
+        }
+
+        if (code.type == 'youtubePlaylist') {
+            e = document.createElement('iframe')
+
+            e.setAttribute('src', this.slugTpl.youtubePlaylist.replace('{slug}', code.slug))
+        }
+
+        // POST-PROCESS if target node is given
+        if (e && targetNode) {
+            // remove selector css class from targetNode
+            targetNode.classList.remove(this.selector.substring(1))
+
+            // add helper css classes but keep those from targetNode
+            e.classList.add('lazymedia', code.type, ...targetNode.classList)
 
             // keep targetNode style
             let style = targetNode.getAttribute('style')
@@ -246,104 +177,53 @@
 }
 
 
-interface LazyMediaInterface {
-    selector: string
-    slugSuffix: {
-        generic: {
-            link: string
-            image: string
-            audio: string
-            video: string
-        }
-        bandcamp: {
-            track: string
-            album: string
-        }
-        mixcloud: {
-            mix: string
-            playlist: string
-        }
-        youtube: {
-            video: string
-            playlist: string
-        }
+
+
+
+
+
+
+
+
+
+
+
+
+function getAudioTypeByExt(filename: string): string | null {
+    let audioType = null
+    let audioExt = filename.split('.').pop()
+
+    if (audioExt == 'mp3') {
+        audioType = 'audio/mpeg'
     }
-    bandcampAlbumAutoHeight: {
-        header: number,
-        trackRow: number,
-        bottomBar: number,
+
+    if (audioExt == 'ogg') {
+        audioType = 'audio/ogg'
     }
-    embed(): void
-    createDefaultElement(tag: string, targetNode?: HTMLElement): HTMLElement
+
+    if (audioExt == 'wav') {
+        audioType = 'audio/wav'
+    }
+
+    return audioType
 }
 
 
-type lazyCodeType = {
-    platform: string
-    type: string
-    slug: string
-    attribute?: [string, string][]
-    dataset?: [string, string][]
-    text?: string
-    trackCount?: number
-    noAudioLabel?: boolean
-} | null
+function getVideoTypeByExt(filename: string): string | null {
+    let videoType = null
+    let videoExt = filename.split('.').pop()
 
-
-
-/*
---- BRAIN FOR DOC LATER ---
-
-    Mandatory:
-        all:
-            - platform
-            - type
-            - slug
-
-    Optional:
-        all:
-            - attribute
-            - dataset
-        generic link:
-            - text
-        generic audio:
-            - noAudioLabel
-        bandcamp:
-            - trackCount
-
-    Example: {
-        "platform": "generic",
-        "type": "link",
-        "slug": "//example.org",
-        "text": "custom link text",
-        "attribute": [
-            ["target", "_blank"],
-            ["title", "foobar"]
-        ],
-        "dataset": [
-            ["cow", "moo"],
-            ["cat", "miao"]
-        ]
+    if (videoExt == 'mp4') {
+        videoType = 'video/mp4'
     }
 
-* defaults ...
-    class names existing already in the lazymedia element will be kept.
-    e.g. in <div class="lazymedia foo bar">{...lazycode...}</div> foo and bar classes will be transfered to the embed element.
+    if (videoExt == 'webm') {
+        videoType = 'video/webm'
+    }
 
-    styles existing already in the lazymedia element will be kept.
-    e.g. in <div style="color:red;">{...lazycode...}</div> style will be transfered to the embed element.
+    if (videoExt == 'ogv') {
+        videoType = 'video/ogg'
+    }
 
-    iframes and images:
-        - loading=lazy
-
-    generic:video and generic:audio...
-        - preload=metadata
-        - controls=true
-
-    youtube:video and youtube:playlist
-        - allowfullscreen=true
-
-
-* set an attribute value to "false" to have it removed in case it's one added by default. like controls for video/audio.
-  e.g. ["controls", "false"]
-*/
+    return videoType
+}
