427393d6918be3f488637c15e4b6fc1a1c0c864f
arT2 <60965610+etrusci-org@users.noreply.github.com>
2022-08-23 13:26:43 +0200

    Update MixcloudTool.php

--- php/MixcloudTool.php
+++ php/MixcloudTool.php
@@ -2,40 +2,41 @@
 // ----------------
 // WORK IN PROGRESS
 // ----------------
-// Usage:
-// require_once __DIR__.'/MixcloudTool.php';
-//
-// $MCT = new MixcloudTool();
-// $MCT->cacheDir = __DIR__.'/_test'; # absolute path to cache directory
-// $MCT->cacheTTL = 60; # seconds before getting fresh remote data. 0 = always remote data but store data also in cache file. -1 = disable caching.
-// $MCT->pagingLimit = 20; # 1-20
-// $MCT->pagingDelay = 150_000; # delay between paging results in microseconds
-// print_r($MCT);
-//
-// $user = $MCT->getUser(user: 'spartacus');
-// print_r($user);
-//
-// $cloudcasts = $MCT->getCloudcasts(user: 'spartacus');
-// print_r($cloudcasts);
-// ----------------
 declare(strict_types=1);
 
 
 class MixcloudTool {
-    public string $apiBaseURL = 'https://api.mixcloud.com';
+    public string $baseURL = 'https://api.mixcloud.com';
     public string $cacheDir = __DIR__;
+    public string $errorFile = __DIR__.'/errors.log';
     public int $cacheTTL = 43_200;
     public int $pagingLimit = 20;
     public int $pagingDelay = 150_000;
+    public string $patternUserURL = '%s/%s/?metadata=1';
+    public string $patternUserCacheFile = '%s/mixcloud-%s-user.json';
+    public string $patternCloudcastsURL = '%s/%s/cloudcasts/?limit=%s&offset=0';
+    public string $patternCloudcastsCacheFile = '%s/mixcloud-%s-cloudcasts.json';
+    public string $patternShowURL = '%s/%s/%s/?metadata=1';
+    public string $patternShowCacheFile = '%s/mixcloud-%s-show-%s.json';
     protected array $ram = [];
 
 
-    protected function getData(string $apiURL, string $cacheFile, null|string $mergeKey = null): array {
+    protected function getData(string $url, string $cacheFile, null|string $mergeKey = null): array {
         if (!file_exists($cacheFile) || time() - filemtime($cacheFile) > $this->cacheTTL) {
-            $data = file_get_contents($apiURL);
+            $curl = curl_init($url);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            $data = curl_exec($curl);
+            $curlStatusCode = curl_getinfo($curl, CURLINFO_RESPONSE_CODE);
+
+            if ($curlStatusCode != 200) {
+                $errline = sprintf("URL: %s\nRESPONSE: %s\n%s\n", $url, $data, str_repeat('-', 100));
+                file_put_contents($this->errorFile, $errline, FILE_APPEND | LOCK_EX);
+                return [];
+            }
+
             $data = json_decode($data, true);
 
-            $this->ram['data'] = array_merge($this->ram['data'], ($mergeKey && isset($data[$mergeKey])) ? $data[$mergeKey] : $data);
+            $this->ram = array_merge($this->ram, ($mergeKey && isset($data[$mergeKey])) ? $data[$mergeKey] : $data);
 
             if (isset($data['paging']) && isset($data['paging']['next'])) {
                 usleep($this->pagingDelay);
@@ -43,38 +44,50 @@ class MixcloudTool {
             }
 
             if ($this->cacheTTL >= 0) {
-                file_put_contents($cacheFile, json_encode($this->ram['data'], JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT), LOCK_EX);
+                file_put_contents($cacheFile, json_encode($this->ram, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT), LOCK_EX);
             }
         }
         else {
             $data = file_get_contents($cacheFile);
             $data = json_decode($data, true);
-            $this->ram['data'] = $data;
+            $this->ram = $data;
         }
 
-        return $this->ram['data'];
+        return $this->ram;
     }
 
 
     public function getUser(string $user): array {
-        $this->ram['data'] = [];
+        $this->ram = [];
 
-        $cacheFile = sprintf('%s/mixcloud-user-%s.json', $this->cacheDir, $user);
-        $apiURL = sprintf('%s/%s/?metadata=1', $this->apiBaseURL, $user);
+        $url = sprintf($this->patternUserURL, $this->baseURL, $user);
+        $cacheFile = sprintf($this->patternUserCacheFile, $this->cacheDir, $user);
 
-        $data = $this->getData($apiURL, $cacheFile);
+        $data = $this->getData($url, $cacheFile);
 
         return $data;
     }
 
 
     public function getCloudcasts(string $user): array {
-        $this->ram['data'] = [];
+        $this->ram = [];
 
-        $cacheFile = sprintf('%s/mixcloud-cloudcasts-%s.json', $this->cacheDir, $user);
-        $apiURL = sprintf('%s/%s/cloudcasts/?limit=%s&offset=0', $this->apiBaseURL, $user, $this->pagingLimit);
+        $url = sprintf($this->patternCloudcastsURL, $this->baseURL, $user, $this->pagingLimit);
+        $cacheFile = sprintf($this->patternCloudcastsCacheFile, $this->cacheDir, $user);
 
-        $data = $this->getData($apiURL, $cacheFile, 'data');
+        $data = $this->getData($url, $cacheFile, 'data');
+
+        return $data;
+    }
+
+
+    public function getShow(string $user, string $slug) {
+        $this->ram = [];
+
+        $url = sprintf($this->patternShowURL, $this->baseURL, $user, $slug);
+        $cacheFile = sprintf($this->patternShowCacheFile, $this->cacheDir, $user, $slug);
+
+        $data = $this->getData($url, $cacheFile);
 
         return $data;
     }
