2f86f549e68b83c4a5c4e3af9afb7a1e7df66ca2
arT2 <60965610+etrusci-org@users.noreply.github.com>
2022-08-24 14:36:50 +0200

    Update MixcloudData.php

--- php/MixcloudData.php
+++ php/MixcloudData.php
@@ -5,40 +5,41 @@
 declare(strict_types=1);
 
 
+/**
+ * Get some Mixcloud data and cache it for later.
+ */
 class MixcloudData {
-    public string $cacheDir = __DIR__; // absolute path to the cache directory
-    public int $cacheTTL = 86400 * 3; // seconds to wait before getting fresh remote data
-    public string $errorFile = __DIR__.'/errors.log'; // absolute path to the error log file
-    public float $requestDelay = 0.250; // seconds to wait between api requests
-    public int $pagingLimit = 20; // how many items to request per page (for results with paging)
-    public string $baseURL = 'https://api.mixcloud.com'; // api base url
-    public string $patternUserURL = '%1$s/%2$s/?metadata=1'; // 1=baseURL, 2=user
-    public string $patternUserCacheFile = '%1$s/mixcloud-%2$s-user.json'; // 1=cacheDir, 2=user
-    public string $patternCloudcastsURL = '%1$s/%2$s/cloudcasts/?limit=%3$s&offset=0'; // 1=baseURL, 2=user, 3=pagingLimit
-    public string $patternCloudcastsCacheFile = '%1$s/mixcloud-%2$s-cloudcasts.json'; // 1=cacheDir, 2=user
-    public string $patternShowURL = '%1$s/%2$s/%3$s/?metadata=1'; // 1=baseURL, 2=user, 3=slug
-    public string $patternShowCacheFile = '%1$s/mixcloud-%2$s-show-%3$s.json'; // 1=cacheDir, 2=user, 3=slug
-    protected array $ram = []; // temporary data storage
+    public string $cacheDir = __DIR__; // Absolute path to the cache directory.
+    public int $cacheTTL = 604_800; // Seconds until cached data files expire. 0=always remote data but store data also in cache file. -1=disable caching.
+    public string $errorFile = __DIR__.'/mixcloud-error.log'; // Absolute path to the error log file.
+    public float $requestDelay = 0.250; // Seconds to wait between remote API requests.
+    public int $pagingLimit = 20; // How many items to request per page in a remote API request (for results with paging, e.g. cloudcasts).
+    public string $baseURL = 'https://api.mixcloud.com'; // API base URL without trailing slash.
+    public string $patternUserURL = '%1$s/%2$s/?metadata=1'; // API URL pattern for user. 1=baseURL, 2=user.
+    public string $patternUserCacheFile = '%1$s/mixcloud-%2$s-user.json'; // Cache file pattern for user. 1=cacheDir, 2=user.
+    public string $patternCloudcastsURL = '%1$s/%2$s/cloudcasts/?limit=%3$s&offset=0'; // API URL pattern for cloudcasts. 1=baseURL, 2=user, 3=pagingLimit.
+    public string $patternCloudcastsCacheFile = '%1$s/mixcloud-%2$s-cloudcasts.json'; // Cache file pattern for cloudcasts. 1=cacheDir, 2=user.
+    public string $patternShowURL = '%1$s/%2$s/%3$s/?metadata=1'; // API URL pattern for show. 1=baseURL, 2=user, 3=slug.
+    public string $patternShowCacheFile = '%1$s/mixcloud-%2$s-show-%3$s.json'; // Cache file pattern for show. 1=cacheDir, 2=user, 3=slug.
+    protected array $ram = []; // Temporary data storage used by methods.
 
 
     protected function getData(string $url, string $cacheFile, null|string $mergeKey = null): array {
         if (!file_exists($cacheFile) || time() - filemtime($cacheFile) > $this->cacheTTL) {
-            if (isset($this->ram['nextRequestOn'])) {
-                time_sleep_until($this->ram['nextRequestOn']);
-            }
-
             $curl = curl_init($url);
             curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-            $data = curl_exec($curl);
-            $curlStatusCode = curl_getinfo($curl, CURLINFO_RESPONSE_CODE);
 
-            if ($curlStatusCode != 200) {
-                $errline = sprintf("TIME: %s\nURL: %s\nRESPONSE: %s\n%s\n", date('Y-m-d H:i:s T'), $url, $data, str_repeat('-', 100));
-                file_put_contents($this->errorFile, $errline, FILE_APPEND | LOCK_EX);
+            if (isset($this->ram['nextRequestOn']) && $this->ram['nextRequestOn'] >= microtime(true)) {
+                time_sleep_until($this->ram['nextRequestOn']);
+            }
+            $data = curl_exec($curl);
+            $this->ram['nextRequestOn'] = microtime(true) + $this->requestDelay;
+
+            if (curl_getinfo($curl, CURLINFO_RESPONSE_CODE) != 200) {
+                $errorMsg = sprintf("TIME: %s\nURL: %s\nRESPONSE: %s\n%s\n", date('Y-m-d H:i:s T'), $url, $data, str_repeat('-', 100));
+                file_put_contents($this->errorFile, $errorMsg, FILE_APPEND | LOCK_EX);
                 return [];
             }
-
-            $this->ram['nextRequestOn'] = microtime(true) + $this->requestDelay;
 
             $data = json_decode($data, true);
 
